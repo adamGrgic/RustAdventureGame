747173f895d5dc9457440b39ac6d411bdc31647e
adamGrgic <argrgic@gmail.com>
2024-12-18 22:32:39 -0700

    Brief intro on structs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,37 @@
 use std::io;
 
+struct Option {
+    number: u32,
+    description: String
+}
+
+impl Option {
+    fn new(number: u32, description: &str) -> Self {
+        Self {
+            number,
+            description: description.to_string()
+        }
+    }
+}
+
+struct Champion {
+    name: String,
+    health: u32,
+    attack: u32,
+}
+
+impl Champion {
+    fn new(name: &str, health: u32, attack: u32) -> Self {
+        Self {
+            health,
+            attack,
+            name: name.to_string()
+        }
+    }
+}
+
 fn main() {
+    let _my_option = Option::new(1, "This is an example description");
     println!("Welcome to Underworld Legends!");
 
     println!("Please enter your name .");
@@ -9,9 +40,12 @@ fn main() {
 
     io::stdin()
         .read_line(&mut name)
-        .expect("Failed to read line");
+        .expect("Failed to read line.");
 
-    println!("Welcome {}", name);
+
+
+    let champion = Champion::new(&name, 100, 10);
+    println!("Welcome {}", champion.name);
 
     let intro = r#"
     You wake up dazed and confused. Unsure of how you got here, you
@@ -29,6 +63,18 @@ fn main() {
 
     for &option in &options {
         println!("{}", &option);
+    }
+
+    let mut user_choice = String::new();
+
+    io::stdin()
+        .read_line(&mut user_choice)
+        .expect("Failed to read option.");
+
+    if options.contains(&user_choice.trim()) {
+        println!("User chose a valid option.")
+    } else {
+        println!("User did not choose a valid option.")
     }
 
 }
